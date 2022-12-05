ac0bab76b82569616fb71172bffd4608124c88d3
anVlad11 <anvlad11@gmail.com>
2022-12-05 12:27:40 +0300

    finished day five part two

--- cmd/day_5/main.go
+++ cmd/day_5/main.go
@@ -143,6 +143,7 @@ func part1(file []string) error {
 		}
 		fmt.Print(stack[0])
 	}
+	fmt.Println()
 
 	return nil
 }
@@ -150,6 +151,82 @@ func part1(file []string) error {
 func part2(file []string) error {
 	if len(file) == 0 {
 		return nil
+	}
+
+	stacks := map[int][]string{}
+
+	i := 0
+	for ; i < len(file); i++ {
+		line := file[i]
+		if strings.HasPrefix(line, " 1") {
+			i = i + 2
+			break
+		}
+
+		column := 0
+		for j := 1; j < len(line); j = j + 4 {
+			column++
+			val := string(line[j])
+			if val == " " {
+				continue
+			}
+
+			if _, exists := stacks[column]; !exists {
+				stacks[column] = []string{val}
+			} else {
+				stacks[column] = append(stacks[column], val)
+			}
+		}
+	}
+
+	moveRegexp, err := regexp.Compile(`move (\d*) from (\d*) to (\d*)`)
+	if err != nil {
+		return err
+	}
+
+	for ; i < len(file); i++ {
+		line := file[i]
+
+		values := moveRegexp.FindAllStringSubmatch(line, -1)
+		if len(values) == 0 || len(values[0]) != 4 {
+			continue
+		}
+
+		amount, _ := strconv.Atoi(values[0][1])
+		from, _ := strconv.Atoi(values[0][2])
+		to, _ := strconv.Atoi(values[0][3])
+
+		if _, exists := stacks[to]; !exists {
+			stacks[to] = []string{}
+		}
+
+		leftPart := make([]string, len(stacks[from][0:amount]))
+		for j, s := range stacks[from][0:amount] {
+			leftPart[j] = s
+		}
+
+		rightPart := make([]string, len(stacks[to]))
+		for j, s := range stacks[to] {
+			rightPart[j] = s
+		}
+
+		// stacks[to] = append(stacks[from][0:amount], stacks[to]...) won't work
+		stacks[to] = append(leftPart, rightPart...)
+		if len(stacks[from]) == amount {
+			stacks[from] = []string{}
+		} else {
+			stacks[from] = stacks[from][amount:]
+		}
+	}
+	for column := 1; ; column++ {
+		stack, exists := stacks[column]
+		if !exists {
+			break
+		}
+		if len(stack) == 0 {
+			continue
+		}
+		fmt.Print(stack[0])
 	}
 
 	return nil
